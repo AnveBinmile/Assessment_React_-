7ba104ddd37baf022ce258e9e8ec42b38903dbca
anvesha <anvesha@binmile.com>
2023-09-04 16:36:51 +0530

    changes pushed initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Content from "./Components/Content/Content";
+import ComponentNavbar from "./Components/ComponentNavbar/ComponentNavbar";
+import {useState } from "react";
+
+import AppContext from "./Components/Context/AppContext";
 
 function App() {
+  const [showSideNav, setShowSideNav] = useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContext.Provider
+      value={{
+        favArray: [],
+        showSideNav: true,
+        setShowSideNav: setShowSideNav,
+      }}
+    >
+      <div className="App bg-black flex justify-center sm:justify-start">
+        {showSideNav && <ComponentNavbar />}
+        <Content />
+      </div>
+    </AppContext.Provider>
   );
 }
+
 
 export default App;
